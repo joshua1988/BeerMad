4104285ef19dfc788a5c9a18ce301a1a8179f9c6
joshua1988 <jangkeehyo@gmail.com>
2016-12-16 17:50:17 +0900

    Changed the file path to get all scripts and libraries

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 // Body Parser MW
 app.use(bodyParser.json());
